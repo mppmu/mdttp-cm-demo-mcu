baf9025c0b4de4ce34c018838aef2bc408c3ea29
Markus Fras <fras@mpp.mpg.de>
2021-03-25 12:35:38 +0100

    Script for configuring the clocks for IBERT tests added. The reference clock for FELIX-related links now defaults to 240 MHz.

---
+++ Software/HwTest/pyMcu/script/setup_ibert.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# File: setup_ibert.sh
+# Auth: M. Fras, Electronics Division, MPI for Physics, Munich
+# Mod.: M. Fras, Electronics Division, MPI for Physics, Munich
+# Date: 23 Mar 2021
+# Rev.: 23 Mar 2021
+#
+# Simple script to set up the ATLAS MDT Trigger Processor (TP) Command Module
+# for Xilinx IBERT tests.
+#
+
+
+
+cd `dirname $0`
+cd ..
+
+
+
+PY_MCU_CM="./pyMcuCm.py"
+SERIAL_DEVICE="/dev/ttyUL1"
+VERBOSITY="0"
+
+
+
+echo "Power up the Command Module."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c power_up
+
+echo "Program the clock synthesizer chip IC60 (Si5345A) for the sector logic (SL) communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC60 config/clock/IBERT-Test/IC60_h6B_FreeRun_O-100M-Registers.txt
+
+echo "Program the clock synthesizer chip IC61 (Si5342A) for the neighbor exchange (CM2CM) communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC61 config/clock/IBERT-Test/IC61_h68_FreeRun_O-100M-Registers.txt
+
+echo "Program the clock synthesizer chip IC62 (Si5345A) for the KU15P-ZU11EG (C2M) communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC62 config/clock/IBERT-Test/IC62_h69_FreeRun_O-100M-Registers.txt
+
+echo "Program the clock synthesizer chip IC63 (Si5345A) for the SFP+ / legacy TTC communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC63 config/clock/IBERT-Test/IC63_h6A_FreeRun_O-100M-Registers.txt
+
+echo "Program the clock synthesizer chip IC82 (Si5344A) for the FELIX communication."
+#${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC82 config/clock/IBERT-Test/IC82_h6A_FreeRun_O-100M-Registers.txt
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC82 config/clock/IBERT-Test/IC82_h6A_FreeRun_O-240M-Registers.txt
+
+echo "Program the clock synthesizer chip IC84 (Si5345A) for the front-end (FE) communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC84 config/clock/IBERT-Test/IC84_h69_FreeRun_O-100M-Registers.txt
+
+echo "Program the clock synthesizer chip IC85 (Si5345A) for the front-end (FE) communication."
+${PY_MCU_CM} -d ${SERIAL_DEVICE} -v ${VERBOSITY} -c clk_setup -p IC85 config/clock/IBERT-Test/IC85_h6A_FreeRun_O-100M-Registers.txt
+
