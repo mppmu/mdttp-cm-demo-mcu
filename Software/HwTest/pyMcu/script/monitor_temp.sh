57ab0dad685c454bcb4bc2ef3fe03a3e28140931
Markus Fras <fras@mpp.mpg.de>
2021-03-26 21:58:37 +0100

    Updated the script 'monitor_temp.sh'.

--- Software/HwTest/pyMcu/script/monitor_temp.sh
+++ Software/HwTest/pyMcu/script/monitor_temp.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# File: monitor_power_temp.sh
+# File: monitor_temp.sh
 # Auth: M. Fras, Electronics Division, MPI for Physics, Munich
 # Mod.: M. Fras, Electronics Division, MPI for Physics, Munich
 # Date: 02 Sep 2020
-# Rev.: 04 Sep 2020
+# Rev.: 26 Mar 2021
 #
-# Simple script to monitor power and temperatures of the ATLAS MDT Trigger
-# Processor (TP) Command Module.
+# Simple script to monitor temperatures of the ATLAS MDT Trigger Processor (TP)
+# Command Module.
 #
 
 
@@ -19,7 +19,6 @@ cd ..
 while [ 1 ]; do
 #    date +"%Y%m%d %H%M%S"
     date +"%d.%m.%Y %H:%M:%S"
-#    ipmitool -H 192.168.0.2 -P "" -t 0x92 sensor | grep "PIM400KZ Current"
     ./pyMcuCm.py -d "/dev/ttyUL1" -v0 -c mon_temp | grep "KU15P\|ZU11EG"
     echo
 done
